973458331d59f3f7dddb86bbd81bf0888fe1e242
gregatgit <gregdd@outlook.com>
2021-09-07 15:37:44 +1000

    add todos file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Todos from './Todos/Todos';
 
 function App() {
   return (
     <div className="App">
       <h1>Playground</h1>
+      <Todos />
     </div>
   );
 }
---
+++ src/Todos/Todos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <sub>A basic todo list using hooks and context</sub>
+    </div>
+  )
+}
+
+export default Todos
